e9f4338969e3a019323aa09446bba42bfd4fab09
Ayoub <aitayoub1002@gmail.com>
2022-05-22 12:59:32 +0100

    review completed

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -10,116 +10,105 @@ const asyncHandler = require("express-async-handler");
 // product page
 // Creer une review
 // access  Private
-const createProductReview =asyncHandler( async (req, res) => {   
-    const { idProduct,rating, comment } = req.body;
-    if( !rating || !comment){
-      res.status(400);
-      throw new Error("please fill all fields");
-    }
-  
-    const userReview = await Review.find({user:req.user.id,product:idProduct});
+const createProductReview = asyncHandler(async (req, res) => {
+  const { idProduct, rating, comment } = req.body;
+  if (!rating || !comment) {
+    res.status(400);
+    throw new Error("please fill all fields");
+  }
+
+  const userReview = await Review.find({ user: req.user.id, product: idProduct });
 
 
-    if (userReview[0]) {     
+  if (userReview[0]) {
+    res.status(400)
+    throw new Error("you already reviewed this product");
+  } else {
+    const review = new Review({
+      name: req.user.name,
+      rating: rating,
+      comment: comment,
+      user: req.user.id,
+      product: idProduct
+    });
+    review.save((error) => {
+      if (error) {
         res.status(400)
-        throw new Error("you already reviewed this product");
-    }else{
-      const review = new Review({
-        name: req.user.name,
-        rating: rating,
-        comment: comment,
-        user: req.user.id,
-        product:idProduct
-      });
-      review.save((error) => {
-        if (error) {
-            res.status(400)
-            throw new Error( "review not added");
-        } else {
+        throw new Error("review not added");
+      } else {
 
-          //product.numReviews = product.reviews.length;
-  
-          // product.rating =
-          //   product.reviews.reduce((acc, item) => item.rating + acc, 0) /
-          //   product.reviews.length;
+        //product.numReviews = product.reviews.length;
 
-            res.status(200).json({ message: "review added" });
-        }
+        // product.rating =
+        //   product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+        //   product.reviews.length;
+
+        res.status(200).json({ message: "review added" });
+      }
     });
 
-    }
-  
-     
-        
-      
-
-});
-
-// product page
-// Creer une review
-// access  Private
-const deleteProductReview =asyncHandler( async (req, res) => {
-
-  const {idProduct,idReview} = req.query;
-  const product = await Product.findById(idProduct);
-  const review = await Review.findById(idReview)
-  if(product){
-
-    Review.findByIdAndRemove(idReview, (error)=>{
-
-        if(error){
-          res.status(404)
-          throw new Error( "review  not found");
-        }
-        else {
-          res.status(200).json({ message: "review not found" });
-        }
-      } 
-      )
-    product.reviews.pull(review)
-
-    
   }
 
 
-  
 
 
+
+});
+
+// product page
+// Creer une review
+// access  Private
+const deleteProductReview = asyncHandler(async (req, res) => {
+
+  const { _id } = req.query;
+  Review.findByIdAndRemove(_id, (error) => {
+    if (error) {
+      res.status(404)
+      throw new Error("review  not found");
+    }
+    else {
+      res.status(200).json({ message: "review deleted" });
+    }
+  }
+  )
 });
 
 
 // product page
 // Creer une review
 // access  Private
-const getAllReviews =asyncHandler( async (req, res) => {
-  const {idProduct,page,limit} = req.query;
+const getAllReviews = asyncHandler(async (req, res) => {
+  const { idProduct, page, limit } = req.query;
 
   const options = {
-      page: parseInt(page) || 1,
-      limit: parseInt(limit) || 3,
+    page: parseInt(page) || 1,
+    limit: parseInt(limit) || 3,
+    sort: {createdAt:-1}
+
   };
-  const reviews = await Review.paginate({product:idProduct},options)
+  const reviews = await Review.paginate({ product: idProduct }, options)
 
   if (reviews.docs.length !== 0) {
     res.status(200).json(reviews);
   } else {
-      res.status(404)
-      throw new Error("No reviews found")
+    res.status(404)
+    throw new Error("No reviews found")
   }
-  
+
 });
 
 // product page
 // Creer une review
 // access  Private
 const updateProductReview = async (req, res) => {
-    
+
 };
 
 
 
 
 module.exports = {
-    createProductReview,
-    getAllReviews
+  createProductReview,
+  getAllReviews,
+  deleteProductReview
 }
--- src/routes/reviewRoutes.js
+++ src/routes/reviewRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {createProductReview, getAllReviews} = require("../controllers/reviewController")
+const {createProductReview, getAllReviews, deleteProductReview} = require("../controllers/reviewController")
 const router = express.Router();
 const { loginRequire } = require("../middlewares/authMiddleware");
 
@@ -12,6 +12,7 @@ const { loginRequire } = require("../middlewares/authMiddleware");
 
 //page product 
 router.route("/add").post(loginRequire,createProductReview);
+router.route("/delete").delete(deleteProductReview);
 router.route("/all").get(getAllReviews);
 
 
